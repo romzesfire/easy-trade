9fb5dbcdec187ff0264601fdb8734e2087ad6e0b
Roman <belousov.r@helix.ru>
2023-01-26 23:36:55 +0300

    Add mapping Properties on columns

--- EasyTrade/EasyTrade.DAL/DatabaseContext/EasyTradeDbContext.cs
+++ EasyTrade/EasyTrade.DAL/DatabaseContext/EasyTradeDbContext.cs
@@ -11,30 +11,69 @@ public interface IEasyTradeDbContext
     public ClientCurrencyTrade GetTrade(uint id);
 }
 
-public class EasyTradeDbContext : DbContext, IEasyTradeDbContext
+public class EasyTradeDbContext : DbContext
 {
-    private string _connectionString;
-    private DbSet<ClientCurrencyTrade> _trades; 
-    
-    public EasyTradeDbContext(IOptions<DbConfigutation> config)
+    private DbSet<ClientCurrencyTrade> _clientTrades { get; set; } = null!;
+    private DbSet<BrokerCurrencyTrade> _brokerTrades { get; set; } = null!;
+    private DbSet<Currency> _currencies { get; set; } = null!;
+    public EasyTradeDbContext(DbContextOptions<EasyTradeDbContext> options) : base(options)
     {
-        _connectionString = config.Value.ConnectionString;
+        
     }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        optionsBuilder.UseNpgsql(_connectionString);
+        modelBuilder.Entity<ClientCurrencyTrade>().ToTable("client_trade");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.Id).HasColumnName("id");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.BuyAmount).HasColumnName("buyamount");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.SellAmount).HasColumnName("sellamount");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.BrokerCurrencyTradeId)
+            .HasColumnName("brokercurrencytradeid");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.BuyCcyId)
+            .HasColumnName("buyccyid");
+        modelBuilder.Entity<ClientCurrencyTrade>().Property(p => p.SellCcyId)
+            .HasColumnName("sellccyid");
+
+        modelBuilder.Entity<BrokerCurrencyTrade>().ToTable("broker_trade");
+        modelBuilder.Entity<BrokerCurrencyTrade>().Property(p => p.Id).HasColumnName("id");
+        modelBuilder.Entity<BrokerCurrencyTrade>().Property(p => p.BuyAmount).HasColumnName("buyamount");
+        modelBuilder.Entity<BrokerCurrencyTrade>().Property(p => p.SellAmount).HasColumnName("sellamount");
+        modelBuilder.Entity<BrokerCurrencyTrade>().Property(p => p.BuyCcyId)
+            .HasColumnName("buyccyid");
+        modelBuilder.Entity<BrokerCurrencyTrade>().Property(p => p.SellCcyId)
+            .HasColumnName("sellccyid");
+        
+         modelBuilder.Entity<Currency>().ToTable("currency");
+        modelBuilder.Entity<Currency>().Property(p => p.Id).HasColumnName("id");
+        modelBuilder.Entity<Currency>().Property(p => p.Name).HasColumnName("name");
+        modelBuilder.Entity<Currency>().Property(p => p.IsoCode).HasColumnName("isocode");
+
     }
 
-    public void AddTrade(ClientCurrencyTrade currencyTrade)
+    public ClientCurrencyTrade AddTrade(ClientCurrencyTrade currencyTrade)
     {
-        _trades.AddRange(currencyTrade);
+        var brokerTrade = currencyTrade.BrokerCurrencyTrade;
+        currencyTrade.BrokerCurrencyTrade = null;
+        brokerTrade.BuyCcy = null;
+        brokerTrade.SellCcy = null;
+        currencyTrade.BuyCcy = null;
+        currencyTrade.SellCcy = null;
+        _brokerTrades.AddRange(brokerTrade);
         SaveChanges();
+        currencyTrade.BrokerCurrencyTradeId = brokerTrade.Id;
+        _clientTrades.AddRange(currencyTrade);
+        SaveChanges();
+        return currencyTrade;
+    }
+    public IEnumerable<Currency> GetCurrencies()
+    {
+        return _currencies.ToList();
     }
 
-    public ClientCurrencyTrade GetTrade(uint id)
+    public List<ClientCurrencyTrade> GetTrades()
     {
-        var trade = _trades.FirstOrDefault(t => t.Id == id);
-        return trade;
+        return _clientTrades.Include(t => t.BrokerCurrencyTrade)
+            .Include(t => t.BuyCcy)
+            .Include(t => t.SellCcy).ToList();
     }
 }
