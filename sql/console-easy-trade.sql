6c4b690f2ee58b4c46f40a12c668cd509938e62f
Roman <belousov.r@helix.ru>
2023-02-10 22:10:30 +0300

    Fix sql

--- sql/console-easy-trade.sql
+++ sql/console-easy-trade.sql
@@ -26,18 +26,37 @@ CREATE TABLE currency(
     IsoCode CHAR(3) UNIQUE
 );
 
-SELECT * FROM "_currencies";
-SELECT * FROM "_brokerTrades";
+SELECT * FROM currencies;
+SELECT * FROM brokerTrades;
 
-SELECT * FROM "_clientTrades";
+SELECT * FROM balances;
 
-DROP TABLE "_currencies";
-DROP TABLE "_brokerTrades";
-DROP TABLE "_clientTrades";
+DROP TABLE currencies;
+DROP TABLE "brokerTrades";
+DROP TABLE "clientTrades";
+DROP TABLE balances;
 
 INSERT INTO broker_trade(BuyAmount, SellAmount, BuyCcyId, SellCcyId) VALUES (10, 600, 2, 1);
 INSERT INTO client_trade(BrokerCurrencyTradeId, BuyAmount, SellAmount, BuyCcyId, SellCcyId) VALUES (2, 10, 600, 2, 1);
 
-INSERT INTO "_currencies" ("Name", "IsoCode") VALUES ('Russian rouble', 'RUB');
-INSERT INTO "_currencies" ("Name", "IsoCode") VALUES ('Dollar USA', 'USD');
-INSERT INTO "_currencies" ("Name", "IsoCode") VALUES ('Euro', 'EUR');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Russian rouble', 'RUB');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Dollar USA', 'USD');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Pound sterling', 'GBP');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Euro', 'EUR');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Japanese Yen', 'JPY');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Australian dollar', 'AUD');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Dollar canadien', 'CAD');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Swiss franc', 'CHF');
+INSERT INTO currencies ("Name", "IsoCode") VALUES ('Yuan', 'CNH');
+INSERT INTO coefficients ("Operation", "Coefficient") VALUES (0, 1.2);
+
+
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (1, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (2, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (3, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (4, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (5, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (6, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (7, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (8, 10000000);
+INSERT INTO balances ("CurrencyId", "Amount") VALUES (9, 10000000);
