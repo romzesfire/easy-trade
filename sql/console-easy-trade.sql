ee0f536bd91646ded3879c9ef2a6613028539b30
Roman <belousov.r@helix.ru>
2023-02-18 22:53:56 +0300

    Change query

--- sql/console-easy-trade.sql
+++ sql/console-easy-trade.sql
@@ -36,38 +36,47 @@ CREATE TABLE coefficients(
 --     FOREIGN KEY ("SecondCcyId") REFERENCES currencies("Id")
 );
 
+CREATE TABLE coefficients(
+    "Id" SERIAL PRIMARY KEY,
+    "DateTime" DATE,
+    "Coefficient" DECIMAL
+--     "FirstCcyId" INTEGER,
+--     "SecondCcyId" INTEGER,
+--     FOREIGN KEY ("FirstCcyId") REFERENCES currencies("Id"),
+--     FOREIGN KEY ("SecondCcyId") REFERENCES currencies("Id")
+);
 
-SELECT * FROM currencies;
+SELECT * FROM "Balances";
 SELECT * FROM "brokerTrades";
-
-SELECT * FROM coefficients;
-
-DROP TABLE coefficients;
-DROP TABLE "brokerTrades";
-DROP TABLE "clientTrades";
-DROP TABLE balances;
-
+SELECT * FROM "Coefficients";
+SELECT * FROM "Currencies";
+DROP TABLE "Currencies";
+DROP TABLE "Coefficients";
+DROP TABLE "BrokerTrades";
+DROP TABLE "ClientTrades";
+DROP TABLE "Balances";
+DELETE FROM "Coefficients" WHERE "Id"= cast(1 as bigint);
 INSERT INTO broker_trade(BuyAmount, SellAmount, BuyCcyId, SellCcyId) VALUES (10, 600, 2, 1);
 INSERT INTO client_trade(BrokerCurrencyTradeId, BuyAmount, SellAmount, BuyCcyId, SellCcyId) VALUES (2, 10, 600, 2, 1);
 
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Russian rouble', 'RUB');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Dollar USA', 'USD');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Pound sterling', 'GBP');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Euro', 'EUR');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Japanese Yen', 'JPY');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Australian dollar', 'AUD');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Dollar canadien', 'CAD');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Swiss franc', 'CHF');
-INSERT INTO currencies ("Name", "IsoCode") VALUES ('Yuan', 'CNH');
-INSERT INTO coefficients ("CurrencyId", "Coefficient") VALUES (0, 1.2);
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Russian rouble', 'RUB');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Dollar USA', 'USD');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Pound sterling', 'GBP');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Euro', 'EUR');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Japanese Yen', 'JPY');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Australian dollar', 'AUD');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Dollar canadien', 'CAD');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Swiss franc', 'CHF');
+INSERT INTO "Currencies" ("Name", "IsoCode") VALUES ('Yuan', 'CNH');
+INSERT INTO "Coefficients" ("Coefficient", "DateTime") VALUES (1.2, now());
 
 
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (1, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (2, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (3, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (4, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (5, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (6, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (7, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (8, 10000000);
-INSERT INTO balances ("CurrencyId", "Amount") VALUES (9, 10000000);
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (1, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (2, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (3, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (4, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (5, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (6, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (7, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (8, 10000000, now());
+INSERT INTO "Balances" ("CurrencyId", "Amount", "DateTime") VALUES (9, 10000000, now());
