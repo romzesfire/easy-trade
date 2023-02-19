6129201eecf4370efaec58651f6baa821cc7f248
Roman <belousov.r@helix.ru>
2023-02-20 02:08:01 +0300

    Mark base class as abstract, add missed validation

--- EasyTrade.DTO/Model/TradeCreationModel.cs
+++ EasyTrade.DTO/Model/TradeCreationModel.cs
@@ -2,13 +2,15 @@ using System.ComponentModel.DataAnnotations;
 
 namespace EasyTrade.DTO.Model;
 
-public class TradeCreationModel
+public abstract class TradeCreationModel
 {
     [MaxLength(3)]
+    [MinLength(3)]
     [Required(ErrorMessage = "Buy currency is required")]
     public string BuyCurrency { get; set; }
     
     [MaxLength(3)]
+    [MinLength(3)]
     [Required(ErrorMessage = "Sell currency is required")]
     public string SellCurrency { get; set; }
     
