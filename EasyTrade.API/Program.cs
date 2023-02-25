ed94cad44fef75dbf63076e2a7d03483d4c60a9d
Roman <belousov.r@helix.ru>
2023-02-25 22:50:46 +0300

    Add Startup class

--- EasyTrade.API/Program.cs
+++ EasyTrade.API/Program.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using EasyTrade.API;
 using EasyTrade.API.Configuration;
 using EasyTrade.API.Extension;
 using EasyTrade.API.Validation;
@@ -15,50 +16,9 @@ var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .Build();
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers().AddJsonOptions(options => 
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-string connectionString = configuration.GetConnectionString("Database");
-var optionsBuilder = new DbContextOptionsBuilder<EasyTradeDbContext>();
-var lockerCfg = configuration.Get<LockerConfiguration>();
-var options = optionsBuilder.UseNpgsql(configuration.GetSection("Database").Get<DbConfigutation>().ConnectionString);
-var dd = options.Options;
-if (lockerCfg.Type == LockerType.Optimistic)
-{
-    builder.Services.AddSingleton<ILocker, OptimisticLocker>();
-}
-else if(lockerCfg.Type == LockerType.Pessimistic)
-{
-    builder.Services.AddSingleton<ILocker, PessimisticLocker>();
-}
-
-builder.Services.AddQuotesProvider(configuration.GetSection("ApiLayer").Get<QuotesApiConfiguration>())
-    .AddValidationOptions()
-    .AddLocalCurrenciesProvider()
-    .AddScoped<IOperationProvider, OperationDbProvider>()
-    .Configure<LockerConfiguration>(configuration.GetSection("Locker"))
-    .AddScoped<ICurrencyTradesProvider, CurrencyTradesDbProvider>()
-    .AddScoped<ICurrenciesProvider, CurrenciesProvider>()
-    .AddScoped<IBalanceProvider, BalanceDbProvider>()
-    .AddDbServices(configuration.GetSection("Database").Get<DbConfigutation>().ConnectionString)
-    .AddScoped<IBrokerCurrencyTradeCreator, BrokerCurrencyTradeCreator>()
-    .AddScoped<IClientCurrencyTradeCreator, ClientCurrencyTradeCreator>()
-    .AddScoped<ICurrencyTradeCoefficientsProvider, CurrencyTradeCoefficientsProvider>();
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-app.UseMiddleware<ExceptionMiddleware>();
-app.Run();
+var startApp = new Startup(configuration);
+startApp.CreateBuilder();
+startApp.AddServices();
+startApp.Build();
+startApp.AddMiddleware();
+startApp.Run();
---
+++ EasyTrade.API/Startup.cs
@@ -0,0 +1,91 @@
+using System.Text.Json.Serialization;
+using EasyTrade.API.Extension;
+using EasyTrade.API.Validation;
+using EasyTrade.DAL.Configuration;
+using EasyTrade.DAL.DatabaseContext;
+using EasyTrade.DTO.Abstractions;
+using EasyTrade.DTO.Model;
+using EasyTrade.Service.Configuration;
+using EasyTrade.Service.Extensions;
+using EasyTrade.Service.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace EasyTrade.API;
+
+public class Startup
+{
+    private IConfiguration _configuration;
+    private WebApplicationBuilder? _builder;
+    private WebApplication _app;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void CreateBuilder(params string[] args)
+    {
+        _builder = WebApplication.CreateBuilder(args);
+    }
+
+    public void AddServices()
+    {
+        _builder.Services.AddControllers().AddJsonOptions(options =>
+            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+        _builder.Services.AddEndpointsApiExplorer();
+        _builder.Services.AddSwaggerGen();
+        string connectionString = _configuration.GetConnectionString("Database");
+        var optionsBuilder = new DbContextOptionsBuilder<EasyTradeDbContext>();
+        var lockerCfg = _configuration.GetSection("Locker").Get<LockerConfiguration>();
+        var options =
+            optionsBuilder.UseNpgsql(_configuration.GetSection("Database").Get<DbConfigutation>().ConnectionString);
+        var dd = options.Options;
+        if (lockerCfg.Type == LockerType.Optimistic)
+        {
+            _builder.Services.AddSingleton<ILocker, OptimisticLocker>();
+        }
+        else if (lockerCfg.Type == LockerType.Pessimistic)
+        {
+            _builder.Services.AddSingleton<ILocker, PessimisticLocker>();
+        }
+
+        _builder.Services.AddQuotesProvider(_configuration.GetSection("ApiLayer").Get<QuotesApiConfiguration>())
+            .AddValidationOptions()
+            .AddLocalCurrenciesProvider()
+            .AddScoped<IOperationProvider, OperationDbProvider>()
+            .Configure<LockerConfiguration>(_configuration.GetSection("Locker"))
+            .AddScoped<ICurrencyTradesProvider, CurrencyTradesDbProvider>()
+            .AddScoped<ICurrenciesProvider, CurrenciesProvider>()
+            .AddScoped<IBalanceProvider, BalanceDbProvider>()
+            .AddDbServices(_configuration.GetSection("Database").Get<DbConfigutation>().ConnectionString)
+            .AddScoped<IBrokerCurrencyTradeCreator, BrokerCurrencyTradeCreator>()
+            .AddScoped<IClientCurrencyTradeCreator, ClientCurrencyTradeCreator>()
+            .AddScoped<ICurrencyTradeCoefficientsProvider, CurrencyTradeCoefficientsProvider>();
+    }
+
+    public void Build()
+    {
+        _app = _builder.Build();
+    }
+
+    public void AddMiddleware()
+    {
+        if (_app.Environment.IsDevelopment())
+        {
+            _app.UseSwagger();
+            _app.UseSwaggerUI();
+        }
+
+        _app.UseHttpsRedirection();
+
+        _app.UseAuthorization();
+
+        _app.MapControllers();
+        _app.UseMiddleware<ExceptionMiddleware>();
+    }
+
+    public void Run()
+    {
+        _app.Run();
+    }
+}
