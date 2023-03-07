317fc5ec7b6746573e68569fa0927f73126e4149
Roman <belousov.r@helix.ru>
2023-03-07 09:17:08 +0300

    Add BrokerCurrencyTradeCreatorTests with Moq

---
+++ EasyTrade.Test/BrokerCurrencyTradeCreatorTests.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using EasyTrade.Domain.Model;
+using EasyTrade.DTO.Model;
+using EasyTrade.Service.Services;
+using EasyTrade.Test.Model;
+using Moq;
+using NUnit.Framework;
+
+namespace EasyTrade.Test;
+
+[TestFixture]
+public class BrokerCurrencyTradeCreatorTests
+{
+    public static IEnumerable SellCases
+    {
+        get
+        {
+            yield return new TestCaseData(new BrokerCurrencySellTradeCreatorModel(
+                new Currency(1, "Dollar USA", "USD"), 
+                new Currency(2, "Russian Rouble", "RUB"),
+                0.8m, 1000m, 800m
+                )
+            );
+            
+            yield return new TestCaseData(new BrokerCurrencySellTradeCreatorModel(
+                    new Currency(2, "Russian Rouble", "RUB"),
+                    new Currency(1, "Dollar USA", "USD"),
+                    65m, 100m, 6500m
+                )
+            );
+        }
+    }
+    
+    public static IEnumerable BuyCases
+    {
+        get
+        {
+            yield return new TestCaseData(new BrokerCurrencyBuyTradeCreatorModel(
+                    new Currency(1, "Dollar USA", "USD"), 
+                    new Currency(2, "Russian Rouble", "RUB"),
+                    0.2m, 1000m, 5000m
+                )
+            );
+            
+            yield return new TestCaseData(new BrokerCurrencyBuyTradeCreatorModel(
+                    new Currency(2, "Russian Rouble", "RUB"),
+                    new Currency(1, "Dollar USA", "USD"),
+                    75m, 15000m, 200m
+                )
+            );
+        }
+    }
+    
+    
+    [Test]
+    [TestCaseSource(nameof(SellCases))]
+    public void SellTradeCreationTest(BrokerCurrencySellTradeCreatorModel creatorModel)
+    {
+        var brokerTradeCreator = new BrokerCurrencyTradeCreator(creatorModel.QuotesProvider.Object,
+            creatorModel.DomainCalculatorProvider);
+        var trade = brokerTradeCreator.Create(creatorModel.TradeCreationModel, creatorModel.BuyCurrency, creatorModel.SellCurrency);
+        Assert.That(trade.BuyAmount == creatorModel.BuyAmountResult, 
+            $"Broker trade must contain {creatorModel.BuyAmountResult} buy amount, but it was {trade.BuyAmount}");
+    }
+    
+    [Test]
+    [TestCaseSource(nameof(BuyCases))]
+    public void BuyTradeCreationTest(BrokerCurrencyBuyTradeCreatorModel creatorModel)
+    {
+        var brokerTradeCreator = new BrokerCurrencyTradeCreator(creatorModel.QuotesProvider.Object,
+            creatorModel.DomainCalculatorProvider);
+        var trade = brokerTradeCreator.Create(creatorModel.TradeCreationModel, creatorModel.BuyCurrency, creatorModel.SellCurrency);
+        Assert.That(trade.SellAmount == creatorModel.SellAmountResult, 
+            $"Broker trade must contain {creatorModel.SellAmountResult} buy amount, but it was {trade.SellAmount}");
+    }
+}
---
+++ EasyTrade.Test/Model/BrokerCurrencyTradeCreatorModel.cs
@@ -0,0 +1,75 @@
+using EasyTrade.Domain.Model;
+using EasyTrade.Domain.Services;
+using EasyTrade.DTO.Abstractions;
+using EasyTrade.DTO.Model;
+using EasyTrade.Service.Model.ResponseModels;
+using EasyTrade.Service.Services;
+using Moq;
+
+namespace EasyTrade.Test.Model;
+
+public class BrokerCurrencyTradeCreatorModel
+{
+    public Mock<IQuotesProvider> QuotesProvider { get;}
+    public DomainCalculatorProvider DomainCalculatorProvider { get; }
+    public Currency BuyCurrency { get; }
+    public Currency SellCurrency { get; }
+    
+    public BrokerCurrencyTradeCreatorModel(Currency buyCurrency, Currency sellCurrency, 
+        decimal price)
+    {
+        QuotesProvider = new Mock<IQuotesProvider>();
+        QuotesProvider.Setup(q => q.Get(sellCurrency.IsoCode, buyCurrency.IsoCode))
+            .Returns(new Quote(
+                new QuoteResponse()
+                {
+                    Query = new Query() { From = sellCurrency.IsoCode, To = buyCurrency.IsoCode },
+                    Result = price
+                }));
+        BuyCurrency = buyCurrency;
+        SellCurrency = sellCurrency;
+        DomainCalculatorProvider = new DomainCalculatorProvider();
+    }
+}
+
+
+public class BrokerCurrencyBuyTradeCreatorModel : BrokerCurrencyTradeCreatorModel
+{
+    public BuyTradeCreationModel TradeCreationModel { get; set; }
+    public decimal SellAmountResult { get; set; }
+    
+    public BrokerCurrencyBuyTradeCreatorModel(Currency buyCurrency, Currency sellCurrency, 
+        decimal price, decimal buyCount, decimal sellAmountResult) 
+        : base(buyCurrency, sellCurrency, price)
+    {
+        SellAmountResult = sellAmountResult;
+        TradeCreationModel = new BuyTradeCreationModel()
+        {
+            BuyCurrency = buyCurrency.IsoCode,
+            SellCurrency = sellCurrency.IsoCode, 
+            BuyCount = buyCount, 
+            DateTime = DateTimeOffset.Now
+        };
+    }
+}
+
+public class BrokerCurrencySellTradeCreatorModel : BrokerCurrencyTradeCreatorModel
+{
+    public SellTradeCreationModel TradeCreationModel { get; }
+    public decimal BuyAmountResult { get; }
+    public BrokerCurrencySellTradeCreatorModel(Currency buyCurrency, Currency sellCurrency, 
+        decimal price, decimal sellCount, decimal buyAmountResult) 
+        : base(buyCurrency, sellCurrency, price)
+    {
+        BuyAmountResult = buyAmountResult;
+        TradeCreationModel = new SellTradeCreationModel()
+        {
+            BuyCurrency = buyCurrency.IsoCode,
+            SellCurrency = sellCurrency.IsoCode, 
+            SellCount = sellCount, 
+            DateTime = DateTimeOffset.Now
+        };
+    }
+
+    
+}
