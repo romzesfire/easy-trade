ca0eade6c0d2bc12251bb0432894494ed572c7bb
Roman <belousov.r@helix.ru>
2023-04-02 20:03:46 +0300

    Add transaction

--- EasyTrade.Service/Services/Recorder/CurrencyTradeCoefficientRecorder.cs
+++ EasyTrade.Service/Services/Recorder/CurrencyTradeCoefficientRecorder.cs
@@ -3,6 +3,7 @@ using EasyTrade.Domain.Model;
 using EasyTrade.DTO.Abstractions;
 using EasyTrade.DTO.Model;
 using EasyTrade.Service.Services.Cache;
+using System.Transactions;
 
 namespace EasyTrade.Service.Services.Recorder;
 
@@ -25,35 +26,43 @@ public class CurrencyTradeCoefficientRecorder : IDataRecorder<UpdateCurrencyTrad
         _cache = cacheServiceFactory.GetCacheService<CurrencyTradeCoefficient, (string?, string?)>(CacheType.Lock);
     }
 
-    public async Task Record(UpdateCurrencyTradeCoefficientModel data)
+    public async Task Record(UpdateCurrencyTradeCoefficientModel data, Guid username = default)
     {
-        var coefficient = await _coefficients.Get((data.FirstIsoCode, data.SecondIsoCode));
-        if (coefficient.FirstCcy != null && coefficient.SecondCcy != null)
+        await using (var txn = await _db.Database.BeginTransactionAsync())
         {
-            await _locker.ConcurrentExecuteAsync(() =>
+            
+            var coefficient = await _coefficients.Get((data.FirstIsoCode, data.SecondIsoCode));
+            if (coefficient.FirstCcy != null && coefficient.SecondCcy != null)
             {
-                coefficient.Coefficient = data.Coefficient;
-                _db.SaveChanges();
-                _cache.AddOrUpdate((coefficient.FirstCcy.IsoCode, coefficient.SecondCcy.IsoCode), coefficient);
-            }, _lockObject);
-        }
-        else
-        {
-            var firstCcy = await _ccyRepository.Get(data.FirstIsoCode);
-            var secondCcy = await _ccyRepository.Get(data.SecondIsoCode);
-            var c = new CurrencyTradeCoefficient()
+                await _locker.ConcurrentExecuteAsync(() =>
+                {
+                    coefficient.Coefficient = data.Coefficient;
+                    _db.SaveChanges();
+                    txn.Commit();
+                    _cache.AddOrUpdate((coefficient.FirstCcy.IsoCode, coefficient.SecondCcy.IsoCode), coefficient);
+                }, _lockObject);
+            }
+            else
             {
-                FirstCcy = firstCcy,
-                SecondCcy = secondCcy,
-                Coefficient = data.Coefficient,
-                DateTime = data.DateTime
-            };
-            await _locker.ConcurrentExecuteAsync(() =>
-            {
-                _db.Coefficients.Add(c);
-                _db.SaveChanges();
-                _cache.AddOrUpdate((coefficient.FirstCcy.IsoCode, coefficient.SecondCcy.IsoCode), coefficient);
-            }, _lockObject);
+                var firstCcy = await _ccyRepository.Get(data.FirstIsoCode);
+                var secondCcy = await _ccyRepository.Get(data.SecondIsoCode);
+                var c = new CurrencyTradeCoefficient()
+                {
+                    FirstCcy = firstCcy,
+                    SecondCcy = secondCcy,
+                    Coefficient = data.Coefficient,
+                    DateTime = data.DateTime
+                };
+                await _locker.ConcurrentExecuteAsync(() =>
+                {
+                    _db.Coefficients.Add(c);
+                    _db.SaveChanges();
+                    txn.Commit();
+                    _cache.AddOrUpdate((coefficient.FirstCcy.IsoCode, coefficient.SecondCcy.IsoCode), coefficient);
+                }, _lockObject);
+            }
         }
     }
+
+
 }
